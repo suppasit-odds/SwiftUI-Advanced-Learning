83571658deb0577e9a08fcf75bcd8927802000d9
Suppasit <monitorman_sc@hotmail.com>
2024-06-13 09:19:05 +0700

    Create custom Property Wrappers: support combine, asycn/await and  binng

--- SwiftfulThinkingAdvancedLearning/PropertyWrapper2Bootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/PropertyWrapper2Bootcamp.swift
@@ -55,6 +55,94 @@ struct FileManagerValues {
     let userProfile = FileManagerKeypath(key: "user_profile", type: User.self)
 }
 
+import Combine
+
+@propertyWrapper
+struct FileManagerCodableStreamableProperty<T: Codable>: DynamicProperty {
+    @State private var value: T?
+    let key: String
+    private var publisher: CurrentValueSubject<T?, Never>
+    
+    var wrappedValue: T? {
+        get {
+            value
+        }
+        nonmutating set {
+            save(newValue: newValue)
+        }
+    }
+    
+    var projectedValue: CustomProjectedValue<T> {
+        CustomProjectedValue(
+            binding: Binding(
+                get: { wrappedValue },
+                set: { wrappedValue = $0 }
+            ),
+            publisher: publisher
+        )
+    }
+    
+//    var projectedValue: CurrentValueSubject<T?, Never> {
+//        publisher
+//    }
+    
+//    var projectedValue: Binding<T?> {
+//        Binding(
+//            get: { wrappedValue },
+//            set: { wrappedValue = $0 }
+//        )
+//    }
+
+    init(_ key: String) {
+        self.key = key
+        
+        do {
+            let url    = FileManager.documentsPath(key: key)
+            let data   = try Data(contentsOf: url)
+            let object = try JSONDecoder().decode(T.self, from: data)
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("SUCCESS READ")
+        } catch {
+            _value = State(wrappedValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("ERROR READ: \(error)")
+        }
+    }
+    
+    init(_ key: KeyPath<FileManagerValues, FileManagerKeypath<T>>) {
+        let keyPath = FileManagerValues.shared[keyPath: key]
+        let key = keyPath.key
+        
+        self.key = key
+        
+        do {
+            let url    = FileManager.documentsPath(key: key)
+            let data   = try Data(contentsOf: url)
+            let object = try JSONDecoder().decode(T.self, from: data)
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("SUCCESS READ")
+        } catch {
+            _value = State(wrappedValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("ERROR READ: \(error)")
+        }
+    }
+    
+    private func save(newValue: T?) {
+        do {
+            let data = try JSONEncoder().encode(newValue)
+            try data.write(to: FileManager.documentsPath(key: key))
+            value = newValue
+            publisher.send(newValue)
+            print("SUCCESS SAVED")
+        } catch {
+            print("EEROR SAVING: \(error)")
+        }
+    }
+}
+
 @propertyWrapper
 struct FileManagerCodableProperty<T: Codable>: DynamicProperty {
     @State private var value: T?
@@ -135,13 +223,24 @@ struct User: Codable {
     let isPremium: Bool
 }
 
+struct CustomProjectedValue<T: Codable> {
+    let binding: Binding<T?>
+    let publisher: CurrentValueSubject<T?, Never>
+    
+    var stream: AsyncPublisher<CurrentValueSubject<T?, Never>> {
+        publisher.values
+    }
+}
+
+
 struct PropertyWrapper2Bootcamp: View {
     
     //@Capitalize private var title = "hello, world!"
     @Uppercase private var title = "hello, world!"
     //@FileManagerCodableProperty("user_profile") private var userProfile: User?
     //@FileManagerCodableProperty(\.userProfile) private var userProfile: User?
-    @FileManagerCodableProperty(\.userProfile) private var userProfile
+    //@FileManagerCodableProperty(\.userProfile) private var userProfile
+    @FileManagerCodableStreamableProperty(\.userProfile) private var userProfile
 
     var body: some View {
         VStack(spacing: 40) {
@@ -149,7 +248,18 @@ struct PropertyWrapper2Bootcamp: View {
                 title = "new title".capitalized
             }
          
-            SomeBindingView(userProfile: $userProfile)
+            SomeBindingView(userProfile: $userProfile.binding)
+            Button(userProfile?.name ?? "no  value") {
+                userProfile = User(name: "Rickey", age: 1111, isPremium: true)
+            }
+        }
+        .onReceive($userProfile.publisher, perform: { newValue in
+            print("RECEVE NEW VALUE OF: \(newValue)")
+        })
+        .task {
+            for await newValue in $userProfile.stream {
+                print("STREAM NEW VALUE OF: \(newValue)")
+            }
         }
         .onAppear {
             print(NSHomeDirectory())
@@ -163,7 +273,7 @@ struct SomeBindingView: View {
     
     var body: some View {
         Button(userProfile?.name ?? "no  value") {
-            userProfile = User(name: "Jessica", age: 100, isPremium: true)
+            userProfile = User(name: "Beer", age: 100, isPremium: true)
         }
     }
 }
